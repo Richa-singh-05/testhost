ebf8d7474612fc20e73dfd8c5ee2113840816bf0
Richa-singh-05 <79156502+Richa-singh-05@users.noreply.github.com>
2026-03-31 00:31:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import HeroSection from "./components/HeroSection";
+import Leaderboard from "./components/leaderboard";
+import ProcessFlow from "./components/ProcessFlow";
+import Scoreboard from "./components/Scoreboard";
+
+function App() {
+  return (
+  
+  <>
+  <HeroSection/>
+  <ProcessFlow/>
+  <Leaderboard/>
+  <Scoreboard/>
+   
+  </>  
+    );
+}
+
+export default App;
